ce331b8016d8c2db68f6ee75e70199244e36f649
Jong-dae Kim <arcgura@gmail.com>
2017-03-28 23:52:15 +0900

    Update sarco.R

--- sarco.R
+++ sarco.R
@@ -10,12 +10,22 @@ sarcopenia <- read.table("~/R/Sarcopenia.csv", header=TRUE, sep=",", comment.cha
 ## reset number
 sarcopenia[,1]<-c(1:1547)
 
-## divide date column from main data and conversion to date class
-## divide code column from main data
 
 sarco.code<-sarcopenia[c(1,2,11)]
 sarco.date<-sarcopenia[c(1,2,11)]
 
+## remove of unmatching date &  divide code column from main data
+for(i in 12:175)  {
+      if (i%%2 == 1)  {
+            sarco.code<-cbind(sarco.code,sarcopenia[i])
+            for(j in 1:1547) {
+                  if (is.na(sarcopenia[j,i])==TRUE) {
+                        sarcopenia[j,i-1]<-NA
+                  }
+            }
+      }
+
+## divide date column from main data and conversion to date class
 for(i in 12:175 ) {
       if (i%%2 == 0) {
             sarcopenia[[i]]<-as.Date(sarcopenia[[i]], "%d/%m/%Y")
@@ -36,6 +46,10 @@ for(i in 1:nrow(sarco.code) ) {
 CT.number<-sum(sarco.code[,3])
 print(CT.number)
 
+## calculate the !is.na  date
+sum(!is.na(sarco.date[,c(4:85)]))      
+      
+      
 ## calculate the number of patients
 CT.people<-0
 for(i in 1:nrow(sarco.code)) {
