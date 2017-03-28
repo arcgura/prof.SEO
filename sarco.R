be3ff2897c41a49fb3c6065634a0772cc912575b
Jong-dae Kim <arcgura@gmail.com>
2017-03-28 21:10:13 +0900

    sarcopenia

---
+++ sarco.R
@@ -0,0 +1,48 @@
+rm(list=ls())
+
+## read data without exclusion code
+
+codebook <- read.csv("~/R/codebook.csv")
+excode<-codebook$code_for_exclusion
+sarcopenia<-read.table("~/R/Sarcopenia.csv", header = TRUE, sep=",", na.strings = c(excode, "NA","","."), as.is = TRUE )
+
+## reset number
+sarcopenia[,1]<-c(1:1547)
+
+## divide date column from main data and conversion to date class
+## divide code column from main data
+
+sarco.code<-sarcopenia[c(1,11)]
+sarco.date<-sarcopenia[c(1,11)]
+
+for(i in 12:175 ) {
+      if (i%%2 == 0) {
+            sarcopenia[[i]]<-as.Date(sarcopenia[[i]], "%d/%m/%Y")
+            sarco.date<-cbind(sarco.date,sarcopenia[i])
+      }
+      else {
+            sarco.code<-cbind(sarco.code,sarcopenia[i])
+      }
+}
+
+## calculate the number of CT
+for(i in 1:nrow(sarco.code) ) {
+      code.row<-sarco.code[i,]
+      code.row.n<-sum(!is.na(code.row[,c(3:ncol(sarco.code))]))
+      sarco.code[i,2]<-code.row.n
+}
+
+CT.number<-sum(sarco.code[,2])
+print(CT.number)
+
+## calculate the number of patients
+CT.people<-0
+for(i in 1:nrow(sarco.code)) {
+      if (sarco.code[i,2]==0)  {
+            CT.people<-CT.people
+      }
+      else {
+            CT.people<-CT.people + 1
+      }
+}
+print(CT.people)
