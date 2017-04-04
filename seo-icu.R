c4d3942da858377d46a9be846fe0079d5f09283f
Jong-dae Kim <arcgura@gmail.com>
2017-04-04 21:42:00 +0900

    complete
    # <- 손창환교수님 코딩과 비교

--- seo-icu.R
+++ seo-icu.R
@@ -1,22 +1,12 @@
+#read date
 rm(list=ls())
 raw <-read.csv("ICU.csv")
 
-#sec.list<-list()
-#sec <- 1
-#sec.list[[1]]<-data.frame()
-#for (i in 1:467984) {
-#  if (raw$sec[i] < sec) {
-#        sec.list[[sec]]<-rbind(sec.list[[sec]], raw[i,])
-#  }     
-#  else {
-#    sec<-sec+1
-#    sec.list[[sec]]<-data.frame()
-#  }
-#}
-
-
 ### split by sec
+
 sec<-as.integer(raw$sec)
+#sec <- trunc(ICU_monitor_data$sec)
+
 raw$sec<-sec
 s<-split(raw, raw$sec)
 m<-lapply(s, colMeans)
@@ -24,8 +14,31 @@ d<-as.data.frame(m)
 d2<-as.data.frame(t(d))
 row.names(d2)<-d2$sec
 
+#ICP.m<-tapply(raw$ICP, raw$sec, mean)
+#MAP.m<-tapply(raw$MAP, raw$sec, mean)
+#bind<-rbind(ICP.m,MAP.m)
+#bind<-as.data.frame(t(bind))
+#write.csv(mean_file_sec, "C:/Users/AMC/Desktop/ICU monitor data/mean per sec by CHS")
 
-### split by min
 
-split(d2,cut)
+### split by min and correlation 
 
+m<-as.integer(d2$sec/60)
+#min <- trunc(mean_file_sec$sec/60)
+
+min.d<-d2
+min.d$sec<-m
+
+m.split<-split(min.d,min.d$sec)
+m.col<-lapply(m.split, function(x){
+    cor(x[,2], x[,3])
+})
+#correlation_min <- sapply(split(ICU_monitor_data_min, ICU_monitor_data_min$min), function(x){
+# cor(x$ICP, x$MAP, method = "pearson"))  }
+
+m.col.v<-as.numeric(m.col)
+m.col.d<-data.frame(names(m.col), m.col.v)
+names(m.col.d)<-c("minute","correlation")
+
+
+#write.csv(correlation_file_min, "C:/Users/AMC/Desktop/ICU monitor data/correlation coefficient per min by CHS")
