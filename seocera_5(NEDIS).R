df3233259ab7b9bc7d871c1ce450f80f8cb2eea2
Jong-dae Kim <arcgura@gmail.com>
2017-05-26 23:29:59 +0900

    exploration

--- seocera_5(NEDIS).R
+++ seocera_5(NEDIS).R
@@ -1,14 +1,71 @@
 library(dplyr)
+library(ggplot2)
 
-data <- read.table("/Users/jd5/Dropbox/R/seo/data.csv", header = TRUE , sep = ",", na.strings=c("NA","","."), stringsAsFactors = FALSE)
+######## red data
 
+# read NEDIS data
+rawdata <- read.table("/Users/jd5/Dropbox/R/seo/data.csv", header = TRUE , sep = ",", na.strings=c("NA","","."), stringsAsFactors = FALSE)
 umls <- read.table("/Users/jd5/Dropbox/R/seo/umls.txt", header = TRUE, sep="\t", quote = "", strip.white = TRUE, na.strings=c("NA","","."), stringsAsFactors = FALSE)
-
 kcdc <- read.csv("/Users/jd5/Dropbox/R/seo/kcdc7.csv", header = TRUE)
 
 
+# read bestian data
+best <- read.csv("/Users/jd5/Dropbox/R/seo/1602_1704_2.csv", header = TRUE, na.strings = "", stringsAsFactors = FALSE)
+eradm <- read.csv("/Users/jd5/Dropbox/R/seo/1602_1704_ER_adm.csv", header = TRUE, na.strings = "")
+chart_nu <- eradm$chart_nu
+best[best$chart_nu %in% chart_nu, 14] <- "응급의학과"
 
-# umls(주증상) 기준 burn data
+
+# read number of population according to region
+population <- read.csv("/Users/jd5/Dropbox/R/seo/population_city2.csv",   header = TRUE, na.strings = "", stringsAsFactors = FALSE)
+
+
+
+############## cleaning
+
+# change variable names
+names(rawdata)[2] <- "region"
+names(rawdata)[6] <- "age"
+names(rawdata)[10] <- "route"
+names(rawdata)[11] <- "route_vehicle"
+names(rawdata)[12] <- "cause_disease"
+names(rawdata)[13] <- "cause_intention"
+names(rawdata)[14] <- "cause_vector"
+names(rawdata)[25] <- "result_ER"
+
+# conversion 'visit date' to POSIXlt class and make another variables(year, wday, month )      # 계절은?
+yymmdd <- strptime(rawdata$내원일자, "%Y%m%d")
+year <- unclass(yymmdd)$year   ; year
+wday <- unclass(yymmdd)$wday ; wday
+month <- unclass(yymmdd)$mon ; month
+rawdata <- cbind(rawdata, yymmdd, year, wday, month)
+
+# conversion 'visit_time' and make 'hour_data'
+time_temp <- sprintf("%04d", rawdata$내원시간) 
+visit_hour <- format(strptime(time_temp, format="%H%M"), format = "%H" )
+rawdata <- cbind(rawdata, visit_hour)
+
+
+# make 'age group'
+age_group <- floor(rawdata$age / 10) *10
+rawdata <- cbind(rawdata, age_group)
+
+
+# cleaning 'sex'       #1,3,9,5,7 -> Male,    0,2,4,6,8 -> Female
+
+table(rawdata$성별)
+
+sex <- rawdata[, 7]
+sex[(sex == "1" | sex == "3" | sex == "9" | sex == "5" | sex == "7") ] <- "M"
+sex[!(sex == "M")] <- "F"
+
+rawdata <- cbind(rawdata, sex)
+
+
+
+############### filtering burn 
+
+#  burn data according to umls(main symptom)
 burn_umls <- umls[grepl("burn", umls$english) | grepl("Burn", umls$english) | grepl("BURN", umls$english), ]
 burn_umls <- burn_umls[!grepl("burning", burn_umls$english), ]
 burn_umls <- burn_umls[!grepl("Burning", burn_umls$english), ]
@@ -21,9 +78,10 @@ burn_umls <- burn_umls[!grepl("Mc", burn_umls$english), ]
 
 burn_umls_code <- burn_umls$CODE
 
-burndata_umls <- filter(data, 주증상.1 %in% burn_umls_code | 주증상.2 %in% burn_umls_code | 주증상.3 %in% burn_umls_code)
+burndata_umls <- filter(rawdata,  주증상.1 %in% burn_umls_code | 주증상.2 %in% burn_umls_code | 주증상.3 %in% burn_umls_code)
 
-#kcdc7(진단코드) 기준 burn data
+
+#  burn data according to kcdc7(dx code)
 burn_kcdc <- kcdc[grepl("burn", kcdc$english) | grepl("Burn", kcdc$english) | grepl("BURN", kcdc$english), ]
 burn_kcdc <- burn_kcdc[!grepl("due", burn_kcdc$english), ]
 burn_kcdc <- burn_kcdc[!grepl("M", burn_kcdc$code), ]
@@ -33,8 +91,12 @@ burn_kcdc <- burn_kcdc[!grepl("R", burn_kcdc$code), ]
 
 burn_kcdc_code <- burn_kcdc$code
 
-str(data)
-burndata_kcdc <- filter(data, 응급환자퇴실진단코드01 %in% burn_kcdc_code | 응급환자퇴실진단코드02 %in% burn_kcdc_code | 응급환자퇴실진단코드03 %in% burn_kcdc_code )
+burndata_kcdc <- filter(rawdata, 응급환자퇴실진단코드01 %in% burn_kcdc_code | 응급환자퇴실진단코드02 %in% burn_kcdc_code | 응급환자퇴실진단코드03 %in% burn_kcdc_code )
+
+
+#  burn data accroding to injury_vector
+
+burndata_vector <- filter(rawdata, cause_vector == 50)  
 
 
 # find unmatching data
@@ -42,16 +104,39 @@ burndata_umls_unmatch <- filter(burndata_umls, !(응급환자퇴실진단코드0
 burndata_kcdc_unmatch <- filter(burndata_kcdc, !(주증상.1 %in% burn_umls_code))
 
 
+# merge three datasets
+burndata_kcdc_umls <- merge(burndata_kcdc, burndata_umls, all = TRUE)
+burndata <- merge(burndata_kcdc_umls, burndata_vector, all = TRUE)
 
-#merge two dataset
-burndata <- merge(burndata_kcdc, burndata_umls, all = TRUE)
 
-#check duplication
+# check duplication
 sum(duplicated(burndata$X))
 
+# check visit cause
+table(burndata$cause_disease)
 
-# make tidy
-location <- as.data.frame(table(burndata$지역))
+
+# make another 'vector_burn' variable to 'rawdata'
+burn_X <- burndata$X
+vector_burn <- rawdata$X %in% burn_X
+identical(sum(vector_burn), nrow(burndata))
+rawdata <- cbind(rawdata, vector_burn)
+
+
+
+
+
+
+################### exploration    ggplot(mpg, aes(displ, hwy)) + ds       # 산포도 출력
+
+
+# location 
+location <- as.data.frame(table(rawdata$region))  
+location <- arrange(location, Var1)
+population <- arrange(population, 행정구역)
+population_c <- as.numeric(population[,2])
+location <- cbind(location, population_c)
+location <- mutate(location, prevalence = (Freq /population_c*100))
 location
 
 
@@ -102,3 +187,29 @@ trans_in <- as.data.frame(table(burndata$전원.보낸.의료기관종류))
 hospital_from <- c("level1 general H", "general H", "hostpital", "clinic", "oriental clinic", "unknown", "etc")
 trans_in <- cbind(hospital_from, trans_in)
 trans_in
+
+
+
+
+# bestian hospital data
+str(best)
+
+as.data.frame(table(best$sex))
+as.data.frame(table(best$Decade))
+as.data.frame(table(best$adult_child))
+as.data.frame(table(best$city1))
+as.data.frame(table(best$city2))
+as.data.frame(table(best$result))
+as.data.frame(table(best$day_week))
+as.data.frame(table(best$pt_class))
+as.data.frame(table(best$department))
+
+table(best$department, best$result)
+
+as.data.frame(table(best$doctor))
+as.data.frame(table(best$visit_course))
+as.data.frame(table(best$burn_place))
+as.data.frame(table(best$burn_place2))
+as.data.frame(table(best$cause))
+as.data.frame(table(best$cricumstance_of_injury))
+
---
+++ seocera_5_location.R
@@ -0,0 +1,134 @@
+
+library(ggplot2)
+library(dplyr)
+
+location_total <- as.data.frame(table(rawdata$region))
+location_total <- mutate(location_total, percentage = Freq / sum(Freq)*100)
+location_total <- arrange(location_total, Var1)
+population <- arrange(population, 행정구역)
+population_c <- as.numeric(population[,2])
+location_total <- cbind(location_total, population_c)
+location_total <- mutate(location_total, prevalence = (Freq /population_c*100))
+
+location_total
+
+location_burn <- as.data.frame(table(burndata$region))
+location_burn <- mutate(location_burn, percentage = Freq / sum(Freq)*100)
+location_burn <- arrange(location_burn, Var1)
+population <- arrange(population, 행정구역)
+population_c <- as.numeric(population[,2])
+location_burn <- cbind(location_burn, population_c)
+location_burn <- mutate(location_burn, prevalence = (Freq /population_c*100))
+
+location_burn
+
+#### bar plot
+
+#ggplot(data=rawdata, aes(x=region)) +
+#    geom_bar( fill="white", colour="black") 
+
+#ggplot(data=rawdata[vector_burn==TRUE,], aes(x=region)) +
+#    geom_bar( fill="white", colour="black") 
+  
+
+ggplot(data=location_total, aes(x=Var1, y=Freq)) +
+    geom_bar(stat="identity", fill="white", colour="black") +
+    ggtitle("Bar Chart of total visits by Region")
+
+
+ggplot(data=location_burn, aes(x=Var1, y=Freq)) +
+    geom_bar(stat="identity", fill="white", colour="black") +
+    ggtitle("Bar Chart of BURN visits by Region")
+
+
+ggplot(data=location_total, aes(x=Var1, y=prevalence)) +
+    geom_bar(stat="identity", fill="white", colour="black") +
+    ggtitle("Bar Chart of total prevalence by Region")
+
+
+ggplot(data=location_burn, aes(x=Var1, y=prevalence)) +
+    geom_bar(stat="identity", fill="white", colour="black") +
+    ggtitle("Bar Chart of burn
+        prevalence by Region")
+
+
+######## choropleth,    use Kormaps data
+
+require(Kormaps)
+require(tmap)
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(ggmap)
+
+# 윈도우 인코딩문제 때문에 길어진 코드
+tmp <- korpopmap1@data %>% select(21)
+names(tmp) <- c("population")
+tmp$population <- tmp$population / 10^4
+
+# id 값을 .shp 파일과 동일하게 유지 (여기서 id는 각 시도에 대응)
+kor.dat <- data.frame(NAME_1=korpopmap1$name_eng, id=korpopmap1$id)
+kor.dat <- bind_cols(kor.dat, tmp)
+kor.dat
+
+View(kor.dat)
+
+NAME_2 <- c("서울", "부산", "대구", "인천", "광주", "대전", "울산", "경기", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주")
+
+kor.dat <- cbind(kor.dat, NAME_2)
+kor.dat <- arrange(kor.dat, NAME_2)
+id<-kor.dat$id
+location_total <- cbind(id, location_total)
+
+
+# ggplot으로 시각화 위해 데이터프레임으로 변환
+korea.shp.f <- fortify(korpopmap1, region = "id")
+
+merge.shp.coef<-merge(korea.shp.f, location_total, by="id", all.x=TRUE)
+korea.population.2010 <-merge.shp.coef[order(merge.shp.coef$order), ] 
+
+head(korea.population.2010)
+
+
+
+# 1 단계 : 시범 지리정보 도식화
+
+ggplot() +
+    geom_polygon(data = korea.population.2010, 
+        aes(x = long, y = lat, group = group, fill = Freq), 
+        color = "black", size = 0.25) + 
+    coord_map()
+
+# 2 단계 : 배포 품질 도식화
+ggplot() +
+    geom_polygon(data = korea.population.2010, 
+        aes(x = long, y = lat, group = group, fill = Freq), 
+        color = "black", size = 0.25) + 
+    coord_map()+
+    scale_fill_distiller(name="인구 : 단위(만명)", palette = "Greens", breaks = pretty_breaks(n = 10), direction = 1)+
+    theme_nothing(legend = TRUE)+
+    labs(title="ER 방문 환자 수")
+
+ggplot() +
+    geom_polygon(data = korea.population.2010, 
+        aes(x = long, y = lat, group = group, fill = prevalence), 
+        color = "black", size = 0.25) + 
+    coord_map()+
+    scale_fill_distiller(name="퍼센트 : 단위(%)", palette = "Greens", breaks = pretty_breaks(n = 10), direction = 1)+
+    theme_nothing(legend = TRUE) +
+    labs(title="인구 대비 ER 방문 환자 수 (%)")
+
+
+# use ggmap
+
+korea <- qmap("south korea", zoom = 7)
+
+korea +geom_polygon(aes(x=long,y=lat, group=group, alpha=0.25), data=korea.population.2010, fill='white')+
+    geom_polygon(aes(x=long,y=lat, group=group), data=korea.population.2010, color='black', fill=NA)
+
+korea +geom_polygon(aes(x=long,y=lat, group=group,  fill=prevalence), 
+    data=korea.population.2010, color="black") +
+    scale_fill_gradient(low='white', high='red')
+
+korea + geom_polygon(aes(x=long,y=lat, group=group, fill=prevalence), data=korea.population.2010, color='black') +
+    scale_fill_distiller(palette='Spectral') + scale_alpha(range=c(0.5,0.5))
